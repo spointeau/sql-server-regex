e5fe454ade3a1ff121341f472f0ca1b422380247
Sylvain Pointeau <sylvain.pointeau@caudalie.com>
2025-12-18 09:00:13 +0100

    Empty string must not return null, add function IsMatch

--- src/Regex.cs
+++ src/Regex.cs
@@ -8,54 +8,61 @@ using System.Collections.Generic;
 public partial class UDF
 {
     [Microsoft.SqlServer.Server.SqlFunction(IsDeterministic = true)]
-    public static SqlString Match(String input, String pattern)
+    public static SqlBoolean IsMatch(string input, string pattern)
     {
-        if (String.IsNullOrEmpty(input) || String.IsNullOrEmpty(pattern))
+        if (input is null || string.IsNullOrEmpty(pattern))
+        {
+            return new SqlBoolean(false);
+        }
+
+        var success = Regex.IsMatch(input, pattern);
+
+        return new SqlBoolean(success);
+    }
+
+    [Microsoft.SqlServer.Server.SqlFunction(IsDeterministic = true)]
+    public static SqlString Match(string input, string pattern)
+    {
+        if (input is null || string.IsNullOrEmpty(pattern))
         {
             return new SqlString(null);
         }
-        else
-        {
-            Match m = Regex.Match(input, pattern);
 
-            return new SqlString(m.Success ? m.Value : null);
-        }
+        Match m = Regex.Match(input, pattern);
+
+        return new SqlString(m.Success ? m.Value : null);
     }
 
     [Microsoft.SqlServer.Server.SqlFunction(IsDeterministic = true, IsPrecise = true)]
-    public static SqlString GroupMatch(String input, String pattern, String group)
+    public static SqlString GroupMatch(string input, string pattern, string group)
     {
-        if (String.IsNullOrEmpty(input) || String.IsNullOrEmpty(pattern) || String.IsNullOrEmpty(group))
+        if (input is null || string.IsNullOrEmpty(pattern) || string.IsNullOrEmpty(group))
         {
             return new SqlString(null);
         }
-        else
-        { 
-            Group g = Regex.Match(input, pattern).Groups[group];
 
-            return new SqlString(g.Success ? g.Value : null);
-        }
+        Group g = Regex.Match(input, pattern).Groups[group];
+
+        return new SqlString(g.Success ? g.Value : null);
     }
 
     [Microsoft.SqlServer.Server.SqlFunction(IsDeterministic = true, IsPrecise = true)]
-    public static SqlString Replace(String input, String pattern, String replacement)
+    public static SqlString Replace(string input, string pattern, string replacement)
     {
         // the replacement string is not checked for an empty string because that is a valid replacement pattern
-        if (String.IsNullOrEmpty(input) || String.IsNullOrEmpty(pattern) || replacement == null)
+        if (input is null || string.IsNullOrEmpty(pattern) || replacement == null)
         {
             return new SqlString(null);
         }
-        else
-        {
-            return new SqlString(Regex.Replace(input, pattern, replacement));
-        }
+
+        return new SqlString(Regex.Replace(input, pattern, replacement));
     }
 
     [SqlFunction(DataAccess = DataAccessKind.None, FillRowMethodName = "FillMatches", TableDefinition = "Position int, MatchText nvarchar(max)")]
-    public static IEnumerable Matches(String input, String pattern)
+    public static IEnumerable Matches(string input, string pattern)
     {
         List<RegexMatch> MatchCollection = new List<RegexMatch>();
-        if (!String.IsNullOrEmpty(input) && !String.IsNullOrEmpty(pattern))
+        if (input != null && !string.IsNullOrEmpty(pattern))
         {
             //only run through the matches if the inputs have non-empty, non-null strings
             foreach (Match m in Regex.Matches(input, pattern))
@@ -67,13 +74,13 @@ public partial class UDF
     }
 
     [SqlFunction(DataAccess = DataAccessKind.None, FillRowMethodName = "FillMatches", TableDefinition = "Position int, MatchText nvarchar(max)")]
-    public static IEnumerable Split(String input, String pattern)
+    public static IEnumerable Split(string input, string pattern)
     {
         List<RegexMatch> MatchCollection = new List<RegexMatch>();
-        if (!String.IsNullOrEmpty(input) && !String.IsNullOrEmpty(pattern))
+        if (input != null && !string.IsNullOrEmpty(pattern))
         {
             //only run through the splits if the inputs have non-empty, non-null strings
-            String[] splits = Regex.Split(input, pattern);
+            string[] splits = Regex.Split(input, pattern);
             for (int i = 0; i < splits.Length; i++)
             {
                 MatchCollection.Add(new RegexMatch(i, splits[i]));
